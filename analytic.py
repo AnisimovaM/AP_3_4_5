83af96db748b4ba4d2e732606712e1ac7549ab3c
AnisimovaM <146504054+AnisimovaM@users.noreply.github.com>
2024-02-18 00:46:00 +0400

    lab 4.final

--- analytic.py
+++ analytic.py
@@ -69,6 +69,61 @@ def average_monthly_temperature(df: pd.DataFrame, parametr: str) -> pd.Series | 
         return df.groupby(df.Date.dt.month)[parametr].mean()
 
 
+def show_temperature_graph(df: pd.DataFrame, parametr: str) -> None:
+    """
+    Отображение графика температуры за весь период
+    Args:
+      df: Dataframe с исходными значениями
+      parametr: Столбец, указывающий, какая температура берется
+
+    """
+    if parametr in ["Celsius temperature", 'Fahrenheit temperature']:
+        fig = plt.figure(figsize=(30, 5))
+        plt.ylabel(parametr)
+        plt.xlabel("date")
+        plt.title('Изменение температуры')
+        plt.plot(df["Date"], df[parametr], color='green', linestyle='-', linewidth=2)
+        plt.show()
+
+
+def show_monthly_temperature_statistics(df: pd.DataFrame, month: int, year: int) -> None:
+    """
+    Отображение графика температуры за указанный месяц в году
+    и отображение медианы и средних значений
+    Args:
+      df: Dataframe с исходными значениями
+      month: Месяц, для которого строится график температуры
+      year: Год, для которого строится график температуры
+      
+    """
+    month_df = df[(df.Date.dt.month == month) & (df.Date.dt.year == year)]
+    fig = plt.figure(figsize=(30, 5))
+
+    fig.add_subplot(1, 3, 1)
+    plt.ylabel("Celsius temperature")
+    plt.xlabel("Date")
+    plt.plot(month_df.Date.dt.day, month_df["Celsius temperature"],
+             color='green', linestyle='-', linewidth=2, label='Celsius temperature')
+    plt.axhline(y=month_df["Celsius temperature"].mean(), color='yellow', label="Average value")
+    plt.axhline(y=month_df["Celsius temperature"].median(), color='blue', label="Median")
+    plt.legend(loc=2, prop={'size': 10})
+
+    fig.add_subplot(1, 3, 2)
+    plt.ylabel("Fahrenheit temperature")
+    plt.xlabel("Date")
+    plt.plot(month_df.Date.dt.day, month_df["Fahrenheit temperature"],
+             color='red', linestyle='--', linewidth=2, label='Fahrenheit temperature')
+    plt.axhline(y=month_df["Fahrenheit temperature"].mean(), color='yellow', label="Average value")
+    plt.axhline(y=month_df["Fahrenheit temperature"].median(), color='blue', label="Median")
+    plt.legend(loc=2, prop={'size': 10})
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    filtered_temp_df = filter_celsius_temp(df, 25)
+    filtered_date_df = filter_by_date(df, '2023-01-01', '2023-12-31')
+    show_monthly_temperature_statistics(df, 1, 2023)
 
 
 
